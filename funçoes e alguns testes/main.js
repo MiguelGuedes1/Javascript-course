e8e90ba3ff0711954e4d30dbb7be0a3cf31b3648
MiguelGuedes1 <miguelsilvaguedes1@gmail.com>
2023-04-15 02:05:16 +0100

    exercicio completo com funçoes

--- funçoes e alguns testes/main.js
+++ funçoes e alguns testes/main.js
@@ -5,3 +5,48 @@ function ratoentra(){
     mudarcor.setAttribute("style", "background-color: #FF0000;");
 }
 
+
+var numeros_guardados=[]
+var numeros_adicionados=0
+var soma=0
+
+function acrescentarnumero(){
+    var num=document.getElementsByClassName("input")[0]
+    var numero_usuario=Number(num.value)    
+    if(numero_usuario==0){
+        alert("O numero zero nao benefecia nada a analise, escolha outro numero por favor")
+    } 
+    else{
+
+    numeros_guardados.push(numero_usuario) 
+    numeros_adicionados ++    // cada numero que usuario escolher vai ficar guardado no vetor numeros_guardados
+    }
+}
+
+function verificar(){
+
+        mensagem=document.getElementsByClassName("mensagem")[0]
+        var numeromaior= 0
+        var media=""
+
+        soma=0
+
+        for(var i=0;i<numeros_guardados.length;i++){           // for para descobrir qual o numero maior
+       
+            if(numeros_guardados[i]>numeromaior){
+                numeromaior=numeros_guardados[i]
+               }
+            soma=soma+numeros_guardados[i]  
+        }
+
+            media=soma/numeros_adicionados
+            mensagem.innerHTML=`Os numeros que inseriu foram: ${numeros_guardados} <br> A soma de todos os numeros que inseriu é ${soma} <br> O maior numero que inseriu foi o  ${numeromaior} <br> A media dos numeros que inseriu é: ${media.toFixed(2)}`
+
+ }
+      
+        
+     
+
+    
+    
+        
